05b1835863168fa7e27307a2f1e1949b7cc19f30
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2024-10-15 21:47:42 +0800

    Minor changes

--- common/modifier/PermanentModifier.cs
+++
@@ -1,9 +0,0 @@
-using Godot;
-using MonoCustomResourceRegistry;
-
-namespace Game.common.modifier {
-    [RegisteredType(nameof(Modifier), "", nameof(Resource)), GlobalClass]
-    public partial class PermanentModifier : Modifier {
-        public PermanentModifier() : base() {}
-    }
-}
